f82042929931326348ec93877249bbc32401cf57
khrall <raymitoro@gmail.com>
2024-09-28 22:48:02 +0200

    Using keycloak for auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,44 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { KeycloakConfigService } from './config/keycloak-config.service';
 import { GroceryItemsModule } from './grocery-items/grocery-items.module';
 import { ItemTypesModule } from './item-types/item-types.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, GroceryItemsModule, ItemTypesModule],
+  imports: [
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakConfigService,
+      imports: [ConfigModule],
+    }),
+    PrismaModule,
+    GroceryItemsModule,
+    ItemTypesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'log', 'warn', 'error', 'fatal'],
+  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,6 +21,7 @@ async function bootstrap() {
     .setTitle('Ordo')
     .setDescription('The Ordo API description')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
